067dc3fb2d678707d38982ef5d08d2f9be91c169
savik <savik94mail@gmail.com>
2018-05-14 18:10:36 +0300

    added eureka client to parser

--- parser/src/main/java/com/savik/Application.java
+++ parser/src/main/java/com/savik/Application.java
@@ -2,10 +2,12 @@ package com.savik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableDiscoveryClient
 public class Application {
 
     public static void main(String[] args) {
