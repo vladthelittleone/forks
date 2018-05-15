670b36f1388d8ba6fddd3ef85b2f0399dcb45e11
savik <savik94mail@gmail.com>
2018-05-15 17:08:29 +0300

    moved models to domain package

---
+++ domain/src/main/java/com/savik/domain/Team.java
@@ -0,0 +1,35 @@
+package com.savik.domain;
+
+import com.savik.domain.validation.FlashscoreId;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Team {
+
+    @Id
+    @FlashscoreId
+    String flashscoreId;
+
+    @NotNull
+    String name;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    SportType sportType;
+
+}
