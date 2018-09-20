3f6752703f5079dfd46dc1b7eb41ebeefa4047ae
savik <savik94mail@gmail.com>
2018-09-20 11:15:50 +0300

    temporary removed eureka from project

--- engine/src/main/java/com/savik/EngineApplication.java
+++ engine/src/main/java/com/savik/EngineApplication.java
@@ -3,13 +3,14 @@ package com.savik;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableFeignClients
 @EnableScheduling
 @EnableAsync
--- parser/src/main/java/com/savik/ParserApplication.java
+++ parser/src/main/java/com/savik/ParserApplication.java
@@ -2,8 +2,9 @@ package com.savik;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableScheduling
